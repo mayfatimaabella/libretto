4c38ea3401cc2af4efb37a588669c7ba29ce6bdc
mayfatimaabella <mayfatimabella.com>
2025-07-04 02:52:00 -0600

    api.php is not registered in the routes list, added routeserviceprovider

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 // Authentication routes
+Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
