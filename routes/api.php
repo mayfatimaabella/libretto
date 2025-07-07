6db6a70a9f4b3fc35ebd133f00336e90bc037e33
Fatima Abella <114788369+abellafatimay@users.noreply.github.com>
2025-07-07 16:19:12 +0800

    postman still doesnt work

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,17 @@ use App\Http\Controllers\LibrettoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+// Test route
+Route::get('/test', function () {
+    return response()->json(['message' => 'API is working!']);
+});
+
+// Debug AuthController
+Route::get('/debug-auth', function () {
+    $controller = new App\Http\Controllers\AuthController();
+    return response()->json(['message' => 'AuthController loaded successfully']);
+});
+
 // Authentication routes
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
